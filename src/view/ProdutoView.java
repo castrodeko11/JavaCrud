9c415846482fbb4ea76522be5786869d55b6dfe3
castrodeko11 <castrodeko11@hotmail.com>
2019-08-26 18:56:15 -0300

    Use JFrame

---
+++ src/view/ProdutoView.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ProdutoView extends javax.swing.JFrame{
+
+    public ProdutoView (){
+        frameInit();
+        setSize(new Dimension(500, 500));
+        setLocationRelativeTo(null);
+
+    }
+}
